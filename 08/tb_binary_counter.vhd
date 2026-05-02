da23964d647eb54561b0133bd73d7eb4c723abd0
Vedran Vyroubal <vedran.vyroubal@gmail.com>
2026-05-02 17:09:36 +0200

    Poglavlje 8: sekcija binarnog sinkronog brojila
    Dodani: circuit diagram (blok inkrementa), regeneriran timing asset
    s individualnim Q0–Q3 signalima (MSB→LSB). Tekst opisuje carry chain
    jednadžbe i VHDL implementaciju s count+1. Obje jezične verzije.

--- 08/tb_binary_counter.vhd
+++ 08/tb_binary_counter.vhd
@@ -5,13 +5,19 @@ entity tb_binary_counter is
 end entity;
 
 architecture sim of tb_binary_counter is
-    signal clk, rst : std_logic := '0';
-    signal q        : std_logic_vector(3 downto 0);
+    signal clk, rst         : std_logic := '0';
+    signal q_vec            : std_logic_vector(3 downto 0);
+    signal q0, q1, q2, q3  : std_logic;
 
     constant T : time := 10 ns;
 begin
     uut: entity work.binary_counter
-        port map(clk => clk, rst => rst, q => q);
+        port map(clk => clk, rst => rst, q => q_vec);
+
+    q0 <= q_vec(3);
+    q1 <= q_vec(2);
+    q2 <= q_vec(1);
+    q3 <= q_vec(0);
 
     clk <= not clk after T / 2;
 
