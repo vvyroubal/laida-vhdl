c4ec0ee03bb3624d6279b14cd37622f72183ae14
Vedran Vyroubal <vedran.vyroubal@gmail.com>
2026-05-09 17:37:54 +0200

    Poglavlje 9: dodana sekcija posmačnog registra (SISO/SIPO/PISO/PIPO) s shemom i VHDL modelom

---
+++ 09/shift_reg4.vhd
@@ -0,0 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity shift_reg4 is
+    port (
+        clk  : in  std_logic;
+        d_in : in  std_logic;
+        q    : out std_logic_vector(3 downto 0)
+    );
+end entity;
+
+architecture rtl of shift_reg4 is
+    signal reg : std_logic_vector(3 downto 0) := (others => '0');
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            reg <= reg(2 downto 0) & d_in;  -- shift left, new bit enters at LSB
+        end if;
+    end process;
+
+    q <= reg;
+end architecture;
