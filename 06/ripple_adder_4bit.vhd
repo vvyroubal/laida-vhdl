1152f080fbfd81939d907a20222ca9051d8f20b1
Vedran Vyroubal <vedran.vyroubal@gmail.com>
2026-05-27 12:42:09 +0200

    06/ripple_adder_4bit: preimenuj interni full_adder u full_adder_4bit_local
    Datoteka sadrži DVA entitete — full_adder i ripple_adder_4bit. Ime
    'full_adder' kolidiralo je s 06/full_adder.vhd kad se cijelo poglavlje
    analizira u istu work biblioteku (GHDL warning + nedeterministički bind).
    
    Preimenovanje internog razrješava sukob; ripple_adder_4bit sada
    eksplicitno instancira work.full_adder_4bit_local, čime se čuva
    pedagoška vrijednost 'dvije entitete u jednoj datoteci'.

--- 06/ripple_adder_4bit.vhd
+++ 06/ripple_adder_4bit.vhd
@@ -6,14 +6,14 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity full_adder is
+entity full_adder_4bit_local is
     port (
         a, b, c_in : in  std_logic;
         sum, c_out : out std_logic
     );
 end entity;
 
-architecture rtl of full_adder is
+architecture rtl of full_adder_4bit_local is
 begin
     sum   <= a xor b xor c_in;
     c_out <= (a and b) or (c_in and (a xor b));
@@ -36,16 +36,16 @@ end entity;
 architecture strukturni of ripple_adder_4bit is
     signal c : std_logic_vector(3 downto 1);
 begin
-    fa0 : entity work.full_adder
+    fa0 : entity work.full_adder_4bit_local
         port map (a => a(0), b => b(0), c_in => c_in,
                   sum => sum(0), c_out => c(1));
-    fa1 : entity work.full_adder
+    fa1 : entity work.full_adder_4bit_local
         port map (a => a(1), b => b(1), c_in => c(1),
                   sum => sum(1), c_out => c(2));
-    fa2 : entity work.full_adder
+    fa2 : entity work.full_adder_4bit_local
         port map (a => a(2), b => b(2), c_in => c(2),
                   sum => sum(2), c_out => c(3));
-    fa3 : entity work.full_adder
+    fa3 : entity work.full_adder_4bit_local
         port map (a => a(3), b => b(3), c_in => c(3),
                   sum => sum(3), c_out => c_out);
 end architecture;
